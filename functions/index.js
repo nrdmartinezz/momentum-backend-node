bca0451cb8bb5c478c31730440b7dac414456818
Nate <nrd.martinezz@gmail.com>
2025-01-30 12:33:56 -0800

    Fixed Project Structure for Firebase Functions

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,11 @@
 const functions = require("firebase-functions");
 const express = require("express");
-const usersRoutes = require("./api/users");
-
 const app = express();
-app.use(express.json());
 
-// Register user routes
-app.use("/users", usersRoutes);
+// Define your API routes
+app.get("/", (req, res) => {
+  res.send("Hello from Express on Firebase Functions!");
+});
 
+// Expose the Express app as a Cloud Function
 exports.api = functions.https.onRequest(app);
