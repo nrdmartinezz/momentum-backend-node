414388601fe79a20169def907dc84844050d79bf
Nate Martinez <nrd.martinezz@gmail.com>
2025-11-04 18:46:48 -0800

    Remove console.log

--- functions/config/cloudinary.js
+++ functions/config/cloudinary.js
@@ -1,12 +1,6 @@
 const cloudinary = require("cloudinary").v2;
 require("dotenv").config();
 
-
-console.log("Cloudinary Config:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 // Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
